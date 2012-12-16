f1a3245a30ad9057ffcb25796d2cb695a7f94fbb
EC2 Default User <ec2-user@ip-10-49-119-233.eu-west-1.compute.internal>
2012-12-16 20:55:22 +0000

    Consolidacion inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a791d1408ebafd1528d28b575089959169ceabab436e58ddae33766e094329a096b2a386d04b76881dae4711578284df5eb7d7a9bea85f2bba6b40654177f85f'
